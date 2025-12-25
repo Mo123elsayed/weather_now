e5ada817a06ac1dd37bd0eb8587849bb2c2e6f2d
Muhammed Elsayed <133232940+Mo123elsayed@users.noreply.github.com>
2025-12-25 16:52:33 +0200

    `Refactor search screen and form field widgets`

--- lib/features/view/ui/screens/search_screen.dart
+++ lib/features/view/ui/screens/search_screen.dart
@@ -38,42 +38,58 @@ class _SearchScreenState extends State<SearchScreen> {
           icon: const Icon(Icons.arrow_back_ios_new_rounded),
         ),
       ),
-      body: Form(
-        key: _formKey,
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
-          alignment: Alignment.center,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF2196F3), // Blue color at the top
-                Color(0xFF90CAF9), // Lighter blue color at the bottom
-              ],
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Form(
+          key: _formKey,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 10.h),
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF2196F3), // Blue color at the top
+                  Color(0xFF90CAF9), // Lighter blue color at the bottom
+                ],
+              ),
             ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomFormField(controller: searchController),
-              SizedBox(height: 20.h),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(Colors.blueAccent),
-                  padding: WidgetStatePropertyAll(
-                    EdgeInsets.symmetric(horizontal: 40.w, vertical: 12.h),
+            child: Column(
+              children: [
+                SizedBox(height: 10.h),
+                CustomFormField(controller: searchController),
+                SizedBox(height: 20.h),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.blueAccent),
+                    padding: WidgetStatePropertyAll(
+                      EdgeInsets.symmetric(horizontal: 40.w, vertical: 12.h),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Perform search action
+                      context.pushNamed(Routes.resultScreen);
+                    }
+                  },
+                  child: Text('Search', style: AppTextStyle.poppinsWhite20),
+                ),
+                SizedBox(height: 30.h),
+                Align(
+                  heightFactor: 10.5.h,
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Enter a city name to get the weather information.',
+                    textAlign: TextAlign.center,
+                    style: AppTextStyle.quicksandWhite20.copyWith(
+                      fontSize: 16.sp,
+                    ),
                   ),
                 ),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // Perform search action
-                    context.pushNamed(Routes.resultScreen);
-                  }
-                },
-                child: Text('Search', style: AppTextStyle.poppinsWhite20),
-              ),
-            ],
+                SizedBox(height: 20.h),
+              ],
+            ),
           ),
         ),
       ),
--- lib/features/view/ui/widgets/custom_form_field.dart
+++ lib/features/view/ui/widgets/custom_form_field.dart
@@ -9,36 +9,33 @@ class CustomFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: GlobalKey<FormState>(),
-      child: TextFormField(
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        validator: (value) => cityValidation(value),
-        onFieldSubmitted: (value) {
-          if (GlobalKey<FormState>().currentState!.validate()) {
-            // Perform search action
-            context.pushNamed(Routes.resultScreen);
-          }
-        },
-        keyboardType: TextInputType.text,
-        textInputAction: TextInputAction.search,
-        controller: controller,
-        style: const TextStyle(
-          fontSize: 18,
-          color: Colors.black,
-          fontWeight: FontWeight.w500,
-          decoration: TextDecoration.none,
+    return TextFormField(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: (value) => cityValidation(value),
+      onFieldSubmitted: (value) {
+        if (GlobalKey<FormState>().currentState!.validate()) {
+          // Perform search action
+          context.pushNamed(Routes.resultScreen);
+        }
+      },
+      keyboardType: TextInputType.text,
+      textInputAction: TextInputAction.search,
+      controller: controller,
+      style: const TextStyle(
+        fontSize: 15,
+        color: Colors.black,
+        fontWeight: FontWeight.w500,
+        decoration: TextDecoration.none,
+      ),
+      decoration: InputDecoration(
+        hintText: 'Search for a city',
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.r),
+          borderSide: BorderSide.none,
         ),
-        decoration: InputDecoration(
-          hintText: 'Search for a city',
-          filled: true,
-          fillColor: Colors.white,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(30.r),
-            borderSide: BorderSide.none,
-          ),
-          prefixIcon: const Icon(Icons.search),
-        ),
+        prefixIcon: const Icon(Icons.search),
       ),
     );
   }
